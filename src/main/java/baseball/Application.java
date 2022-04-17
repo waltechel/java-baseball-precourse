ce9bb1409713a06209b9deba02f198eaa8540edd
knecht <ldj910505@naver.com>
2022-04-17 16:05:18 +0900

    <feat>(application) : 소스코드 일차적으로 되는 기능을 만드는 데에 집중
    - 일차적으로 되는 기능을 만들었다
    - 되게 하는 데에 목적을 둠
    - 기능 자체는 완성했다고 보여짐

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,127 @@
 package baseball;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	private static String input;
+	private static String answer;
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		sc = new Scanner(System.in);
+		answer = getAnswer();
+
+		do {
+			input = gameplay();
+			int ball = getBall(input, answer);
+			int strike = getStrike(input, answer);
+			print(ball, strike);
+		} while (!isOver(input, answer));
+
+	}
+
+	private static int getStrike(String input, String answer) {
+		int ret = 0;
+		if(input.charAt(0) == answer.charAt(0)) {
+			ret++;
+		}
+		if(input.charAt(1) == answer.charAt(1)) {
+			ret++;
+		}
+		if(input.charAt(2) == answer.charAt(2)) {
+			ret++;
+		}
+		return ret;
+	}
+
+	private static int getBall(String input, String answer) {
+		int ret = 0;
+		if(input.charAt(0) == answer.charAt(1)){
+			ret++;
+		}
+		if(input.charAt(0) == answer.charAt(2)){
+			ret++;
+		}
+		if(input.charAt(1) == answer.charAt(0)){
+			ret++;
+		}
+		if(input.charAt(1) == answer.charAt(2)){
+			ret++;
+		}
+		if(input.charAt(2) == answer.charAt(0)){
+			ret++;
+		}
+		if(input.charAt(2) == answer.charAt(1)){
+			ret++;
+		}
+		return ret;
+	}
+
+	private static void print(int ball, int strike) {
+		if (ball == 0 && strike != 0) {
+			System.out.println(strike + "스트라이크");
+		}
+		if (ball == 0 && strike == 0) {
+			System.out.println("낫싱");
+		}
+		if (ball != 0 && strike == 0) {
+			System.out.println(ball + "볼");
+		}
+		if (ball != 0 && strike != 0) {
+			System.out.println(ball + "볼 " + strike + "스트라이크");
+		}
+	}
+
+	private static boolean isOver(String input, String answer) {
+		return input.equals(answer) && checkRegame();
+	}
+
+	private static boolean checkRegame() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+		int input = sc.nextInt();
+		if(input == 1) {
+			answer = getAnswer();
+		}
+		return input == 2;
+	}
+
+	private static String getAnswer() {
+		String ret = getCandi();
+		while (!isAnswerOkay(ret)) {
+			ret = getCandi();
+		}
+		return ret;
+	}
+
+	private static boolean isAnswerOkay(String candi) {
+		if (candi.length() != 3) {
+			return false;
+		}
+		if (candi.charAt(0) == candi.charAt(1)) {
+			return false;
+		}
+		if (candi.charAt(1) == candi.charAt(2)) {
+			return false;
+		}
+		if (candi.charAt(0) == candi.charAt(2)) {
+			return false;
+		}
+		return true;
+	}
+
+	private static String getCandi() {
+		Random r = new Random();
+		String candi = r.nextInt(999) + "";
+		return candi;
+	}
+
+	private static String gameplay() {
+		System.out.print("숫자를 입력해주세요 : ");
+		String input = sc.next();
+		return input;
+	}
 }
